2cee5cd001b6d3c2608ea4c96904e9a0d77f3520
Paul Thompson <nossidge@gmail.com>
2016-08-24 00:29:49 +0100

    string formatting

--- scripts/database.rb
+++ scripts/database.rb
@@ -16,21 +16,17 @@ $db = SQLite3::Database.new($db_file)
 ################################################################################
 
 def get_users
-  users = []
-  $db.execute("SELECT DISTINCT userName FROM tblFullLogs") do |row|
-    users << row[0]
-  end
-  users.sort
+  $db.execute('SELECT DISTINCT userName FROM tblFullLogs').map(&:first).sort
 end
 
 # Get a hash of user counts by date string in form "yyyymmdd".
 def get_user_count_by_date
   users_by_date = Hash.new { |h, k| h[k] = [] }
-  results = $db.execute(
-      "SELECT strftime('%Y%m%d', timeStamp, 'unixepoch'), " +
-        "lower(userName) FROM tblFullLogs " +
-      "GROUP BY strftime('%Y%m%d', timeStamp, 'unixepoch'), lower(userName)")
-  results.each do |i|
+  $db.execute( %[
+    SELECT strftime('%Y%m%d', timeStamp, 'unixepoch'), lower(userName)
+    FROM tblFullLogs
+    GROUP BY strftime('%Y%m%d', timeStamp, 'unixepoch'), lower(userName)
+  ] ).each do |i|
     users_by_date[ i[0] ] << i[1]
   end
   users_by_date
@@ -39,11 +35,11 @@ end
 # Get a hash of chat line counts by date string in form "yyyymmdd".
 def get_line_count_by_date
   lines_by_date = {}
-  results = $db.execute(
-      "SELECT strftime('%Y%m%d', timeStamp, 'unixepoch'), " +
-        "COUNT(chatText) AS lines FROM tblFullLogs " +
-      "GROUP BY strftime('%Y%m%d', timeStamp, 'unixepoch')")
-  results.each do |i|
+  $db.execute( %[
+    SELECT strftime('%Y%m%d', timeStamp, 'unixepoch'), COUNT(chatText)
+    FROM tblFullLogs
+    GROUP BY strftime('%Y%m%d', timeStamp, 'unixepoch')
+  ] ).each do |i|
     lines_by_date[ i[0] ] = i[1]
   end
   lines_by_date
@@ -52,11 +48,11 @@ end
 # Get a hash of all chat lines by date string in form "yyyymmdd".
 def get_chat_by_date
   chat_by_date = Hash.new { |h, k| h[k] = [] }
-  results = $db.execute(
-      "SELECT strftime('%Y%m%d', timeStamp, 'unixepoch'), " +
-        "timeStamp, userName, chatText " +
-      "FROM tblFullLogs")
-  results.each do |i|
+  $db.execute( %[
+    SELECT strftime('%Y%m%d', timeStamp, 'unixepoch'),
+           timeStamp, userName, chatText
+    FROM tblFullLogs
+  ] ).each do |i|
     chat_by_date[ i[0] ] << [ i[1] , i[2] , i[3] ]
   end
   chat_by_date
